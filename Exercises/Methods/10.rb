e2b3762bb0250c7e800570de5c87d9976774b9d2
Juliana Moscoso <julymm@gmail.com>
2020-03-30 07:04:55 -0400

    add exercises

---
+++ Exercises/Methods/10.rb
@@ -0,0 +1,17 @@
+names = ["Dave", "Sally", "George", "Jessica"]
+activities = ["walking", "running", "cycling"]
+
+def name(names_array)
+  names_array.sample
+end
+
+def activity(activities_array)
+  activities_array.sample
+end
+
+def sentence(name, activity)
+  "#{name} went #{activity} today."
+end
+
+
+puts sentence(name(names), activity(activities))
