aa4417e695976e71a1d1eccd7d9c390607ae00da
Juliana Moscoso <julymm@gmail.com>
2020-04-01 05:46:01 -0400

    add exercises

--- Exercises/Conditionals/8.rb
+++ Exercises/Conditionals/8.rb
@@ -1,11 +1,5 @@
 status = ["awake", "tired"].sample
 
-  # if status == "awake"
-  #   "Be productive!"
-  # else
-  #   "Go to sleep!"
-  # end
-
 output = status == "awake" ? "Be productive!" : "Go to sleep!"
 
 puts output
